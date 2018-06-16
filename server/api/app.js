da2347f3b20670ce520be979b3e602e12f254e76
boneandrea <banchou@peixe.biz>
2018-06-16 12:22:23 +0900

    Set CORS

--- server/api/app.js
+++ server/api/app.js
@@ -23,16 +23,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-app.use('/', usersRouter);
-app.use('/users', usersRouter);
-
 // CORSを許可する
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+
+app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

3621bc6b892cae9777165319f3ac16ef1485b9b1
Your Name <you@example.com>
2019-08-24 09:26:11 +0900

    Config circleCI

--- client/test/unit/jest.conf.js
+++ client/test/unit/jest.conf.js
@@ -1,6 +1,10 @@
 const path = require('path')
 
 module.exports = {
+  verbose: true,
+  testURL: "http://localhost/",
+
+
   rootDir: path.resolve(__dirname, '../../'),
   moduleFileExtensions: [
     'js',
